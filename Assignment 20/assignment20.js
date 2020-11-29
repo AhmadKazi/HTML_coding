be2dff95e1a19b6aa69625f7e5acc3f52b2c7256
AhmadKazi <70097372+AhmadKazi@users.noreply.github.com>
2020-11-29 15:38:14 +0530

    Assignment 20 completed

---
+++ Assignment 20/assignment20.js
@@ -0,0 +1,14 @@
+let first_name = prompt("What is your First Name :");
+let last_name = prompt("What is your Last Name :");
+console.log("My name is", first_name, last_name + ".");
+
+let age = prompt("What is your age :");
+console.log("I am", age, "years old.");
+
+let color = prompt("Which is your favourite color ?");
+console.log("My favourite color is", color + ".");
+
+let hobby = prompt("What is your hobby ?");
+console.log("My Hobby is", hobby + ".");
+
+console.log("Hello! My name is", first_name, last_name + ".", "I am", age, "years old.", "My hobby is", hobby + ";", "and my favourite color is", color + ".", "THANK YOU!!!");
